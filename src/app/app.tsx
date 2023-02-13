0e28f717380c2efd5c5a090e1d24c239fe25631f
Oleksii Vozghorkov <oleksii.vozghorkov@globaltranz.com>
2023-02-13 16:51:54 +0200

    Initial setup

---
+++ src/app/app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from './app.styles';
+
+function App() {
+  const test = () => 111
+
+  return (
+    <Container className="App">
+      <div />
+      <h1>Hello World..!</h1>
+    </Container>
+  );
+}
+
+export default App;
