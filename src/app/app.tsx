9246abdd6e22d4aee106bef350cbdb7ded5c3c0c
Oleksii Vozghorkov <oleksii.vozghorkov@globaltranz.com>
2023-02-20 12:53:33 +0200

    style fixes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Container } from './app.styles';
 
-const App = () => {
-  return (
-    <Container className="App">
-      <div />
-      <h1>Hello World..!</h1>
-    </Container>
-  );
-};
+const App = () => (
+  <Container className="App">
+    <div />
+    <h1>Hello World..!</h1>
+  </Container>
+);
 
 export default App;
