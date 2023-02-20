7ce231f4ec12463798244f9005f2f9765ad82029
Oleksii Vozghorkov <oleksii.vozghorkov@globaltranz.com>
2023-02-20 12:24:16 +0200

    additional files

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,10 @@ module.exports = {
           "css-loader", // for styles
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   devServer: {
